1b1df5196d13640f765b3f81b15ec30be4f5db00
diegoballesterosdev <diegoballesteros.dev@gmail.com>
2024-08-08 15:06:49 -0600

    Initial Commit

---
+++ TestList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestList
+//
+//  Created by Diego Ballesteros on 08/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestList/TestListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestListApp.swift
+//  TestList
+//
+//  Created by Diego Ballesteros on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TestListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
